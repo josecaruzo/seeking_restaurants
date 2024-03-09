9c5b94f702349b9b6767ea08638f38beb22d21c8
Jose Neto <caruzo.neto@gmail.com>
2024-03-09 18:45:34 -0300

    v24.001 - by JN on 09/03/2024 - Creating "Seeking for restaurants" application

---
+++ seeking_restaurants_provider/src/main/java/com/fiap/seeking_restaurants_provider/SeekingRestaurantsProviderApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.seeking_restaurants_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekingRestaurantsProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeekingRestaurantsProviderApplication.class, args);
+	}
+
+}
---
+++ seeking_restaurants_provider/src/test/java/com/fiap/seeking_restaurants_provider/SeekingRestaurantsProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.seeking_restaurants_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekingRestaurantsProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
