09b8670700d2c1fd3b01ceb583f132e82a0abb92
Jose Neto <caruzo.neto@gmail.com>
2024-03-26 19:46:38 -0300

    v24.005 - by JN on 26/03/2024 - adding dockerfile config

---
+++ seeking_restaurants_provider/src/test/java/com/fiap/seeking_restaurants_provider/bdd/CucumberTest.java
@@ -0,0 +1,11 @@
+package com.fiap.seeking_restaurants_provider.bdd;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class CucumberTest {
+}
